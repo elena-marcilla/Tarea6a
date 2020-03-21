df5d0aa0190cc236e129936dde172881ba532e50
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-03-21 17:55:32 +0100

    Update cuatro.sh

--- cuatro.sh
+++ cuatro.sh
@@ -1,7 +1,8 @@
 read -p "Introduzca un valor mayor que 0" v1
-if [ $v1 -lt 0 ]; then
+while [ $v1 -lt 0 ]; do
 echo "Número incorrecto"
 read -p "Introduzca un valor mayor que 0" v1
+done
 for i in `seq 0 $v1`; do
 echo $i
 done
