4dcb1ad87da8baf725f3d5a513f51c9a825d3705
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-03-21 17:35:15 +0100

    Update tres.sh

--- tres.sh
+++ tres.sh
@@ -2,13 +2,13 @@ read -p "Introduzca su nota" nota
 if [ nota -lt 0 || nota -gt 10 ]; then
 echo "Su nota no es válida"
 read -p "Introduzca su nota" nota
-else if [ nota -lt 5 ]; then
+else if [ $nota -lt 5 ]; then
 echo "Suspendido"
-else if [ nota -eq 5 ]; then
+else if [ $nota -eq 5 ]; then
 echo "Aprobado"
-else if [ nota -eq 6 ]; then
+else if [ $nota -eq 6 ]; then
 echo "Bien"
-else if [ nota -eq 7 || nota -eq 8 ]; then
+else if [ $nota -eq 7 || nota -eq 8 ]; then
 echo "Notable"
 else 
 echo "Sobresaliente"
