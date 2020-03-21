c762bba4e5fe6a25058bca8ee8348b9d6d5cc218
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-03-21 17:19:37 +0100

    Create uno.sh

---
+++ uno.sh
@@ -0,0 +1,9 @@
+read -p "introduce un valor" v1
+read -p "introduce otro valor" v2
+if [ v1 -gt v2 ]; then
+echo "el mayor es $v1" 
+else if [ v1 -eq v2 ]; then
+echo "son iguales"
+else 
+echo "el mayor es $v2"
+fi
