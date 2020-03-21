f6b53e57faacb28e227bd3ef91f75a0e0a7baa84
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-03-21 17:38:53 +0100

    Update cuatro.sh

--- cuatro.sh
+++ cuatro.sh
@@ -1,5 +1,5 @@
 read -p "Introduzca un valor mayor que 0" v1
-if [ v1 -lt 0 ]; then
+if [ $v1 -lt 0 ]; then
 echo "Número incorrecto"
 read -p "Introduzca un valor mayor que 0" v1
 for i in `seq 0 $v1`; do
