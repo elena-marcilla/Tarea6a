55ec4ead301bb0458fb984351cfd071b8dd6b8e5
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-03-21 17:38:25 +0100

    Update dos.sh

--- dos.sh
+++ dos.sh
@@ -1,8 +1,8 @@
 read -p "Introduce un valor mayor a 0" v1
-if [ v1 -lt 0 ]; then
+if [ $v1 -lt 0 ]; then
 echo "El valor introducido no es correcto"
 read -p "Introduce un valor mayor a 0" v1
-else if [ v1%2 = 0 ]; then
+else if [ $v1%2 = 0 ]; then
 echo "El número es par"
 else 
 "El número es impar"
