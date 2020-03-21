8fe11a54d40f9bdd87b682d2258ccf8541d467e8
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-03-21 17:30:03 +0100

    Create tres.sh

---
+++ tres.sh
@@ -0,0 +1,15 @@
+read -p "Introduzca su nota" nota
+if [ nota -lt 0 || nota -gt 10 ]; then
+echo "Su nota no es válida"
+read -p "Introduzca su nota" nota
+else if [ nota -lt 5 ]; then
+echo "Suspendido"
+else if [ nota -eq 5 ]; then
+echo "Aprobado"
+else if [ nota -eq 6 ]; then
+echo "Bien"
+else if [ nota -eq 7 || nota -eq 8 ]; then
+echo "Notable"
+else 
+echo "Sobresaliente"
+fi
