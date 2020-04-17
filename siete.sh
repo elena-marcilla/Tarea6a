aa1e881711d5b00d89f912a7a2cdef177f698058
elena-marcilla <58435122+elena-marcilla@users.noreply.github.com>
2020-04-17 22:57:11 +0200

    Autocorrección siete

--- siete.sh
+++ siete.sh
@@ -3,4 +3,13 @@ while [ $valor -lt 1 || $valor -gt 30 ]; do
 echo "Valor inválido"
 read -p "Introduzca un valor entre 0 y 30" valor
 done
-if [ 
+contador = 0
+while [true]; do
+for i in lunes martes miercoles jueves viernes sabado domingo; do
+contador=$((contador+1))
+if [contador = $dia]; then
+echo "El dia es $i"
+exit
+fi
+done
+done
